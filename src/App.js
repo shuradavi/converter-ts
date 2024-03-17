81f41a9f0c9127214de5481289789d729fe26733
shuradavi/macos <shuradavi@gmail.com>
2024-03-17 20:21:59 +0300

    v 0.1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'; 
 import { getData } from './params/params';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const App =  () => {
 	const [inputValue, setInputValue] = useState('');
 	const [outputValue, setOutputValue] = useState('')
+	const defaultCurrencies = ['RUB', 'USD', 'EUR', 'GBP']
+
 	const [inputCurrency, setInputCurrency] = useState('RUB')
 	const [outputCurrency, setOutputCurrency] = useState('USD')
 	
@@ -29,33 +32,59 @@ const App =  () => {
 		setOutputCurrency(event.target.value)
 		console.log('clicked on output', event.target.value);
 	}
-	unnamedFunc('UZS');
+	// unnamedFunc('UZS');
+	// getData().then(res => console.log(Object.keys(res)))
 	return (
-		<div>
-			<div className='converter-wrapper'>
-				<div className='converter-header'>
-					<h1>Конвертер валют</h1>
-				</div>
-				<div className='converter-body'>
-					<div className='input-currency'>
-						<button value={'RUB'} onClick={onInputCurrencyHandler}>RUB</button>
-						<button value={'USD'} onClick={onInputCurrencyHandler}>USD</button>
-						<button value={'EUR'} onClick={onInputCurrencyHandler}>EUR</button>
+		<div className='App'>
+			<h1>Конвертер валют</h1>
+			<div className='converter'>
+				<div className='converter-box-wrapper'>
+					<div className='converter-box'>
+						<div className='converter-side'>
+							<div className='side-title'>У меня есть</div>
+							<div className='side-switcher'>
+								<div className='side-switcher-item active'>RUB</div>
+								<div className='side-switcher-item'>USD</div>
+								<div className='side-switcher-item'>EUR</div>
+								<div className='side-switcher-item'>GBP</div>
+								<div className='side-switcher-item'><FontAwesomeIcon icon="fas fa-angle-down" /></div>
+							</div>
+							<div className='side-input-box'>
+								<input className='input' placeholder='0' maxLength='10' type='text'/>
+								<div className='side-input-box-rate'>курс валюты</div>
+							</div>
+						</div>
+						<div className='converter-center'>SWITCH</div>
+						<div className='converter-side'>
+							<div className='side-title'></div>
+							<div className='side-switcher'></div>
+							<div className='side-input-box'>
+								<input className='input' placeholder='0' maxLength='10' type='text'/>
+								<div className='side-input-box-rate'>курс валюты</div>
+							</div>
+						</div>
 					</div>
-					<div className='input-value'>
-						<input onChange={onInputHandler} value={inputValue} autoFocus/>
-						<div className=''>руб.</div>
-					</div>
-					<div>{`->`}</div>
-					<div className='output-value'>
-					<div className='output-currency'>
-						<button value={'RUB'} onClick={onOutputCurrencyHandler}>RUB</button>
-						<button value={'USD'} onClick={onOutputCurrencyHandler}>USD</button>
-						<button value={'EUR'} onClick={onOutputCurrencyHandler}>EUR</button>
-					</div>
-						<input value={outputValue} readOnly/>
-						<div className=''>дол.</div>
-					</div>
+					{/* <div className='converter-body'>
+						<div className='input-currency'>
+							<button value={'RUB'} onClick={onInputCurrencyHandler}>RUB</button>
+							<button value={'USD'} onClick={onInputCurrencyHandler}>USD</button>
+							<button value={'EUR'} onClick={onInputCurrencyHandler}>EUR</button>
+						</div>
+						<div className='input-value'>
+							<input onChange={onInputHandler} value={inputValue} autoFocus/>
+							<div className=''>руб.</div>
+						</div>
+						<div>{`->`}</div>
+						<div className='output-value'>
+						<div className='output-currency'>
+							<button value={'RUB'} onClick={onOutputCurrencyHandler}>RUB</button>
+							<button value={'USD'} onClick={onOutputCurrencyHandler}>USD</button>
+							<button value={'EUR'} onClick={onOutputCurrencyHandler}>EUR</button>
+						</div>
+							<input value={outputValue} readOnly/>
+							<div className=''>дол.</div>
+						</div>
+					</div> */}
 				</div>
 				<div className='converter-footer'></div>
 			</div>
