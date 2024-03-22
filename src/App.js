8136d7c1f650dae236687d4a7210ccfdc31f1f54
shuradavi <shuradavi@gmail.com>
2024-03-22 03:42:43 +0300

    added sort at currency arr

--- src/App.js
+++ src/App.js
@@ -19,11 +19,12 @@ const App =  () => {
 			.then(res => {
 				console.log('res', res);
 				let defaultCurrencyArray = res.filter(item => item.CharCode === 'USD' || item.CharCode === 'EUR' || item.CharCode === 'GBP')
+				defaultCurrencies.sort((a, b) => a.localeCompare(b))
 				let currenciesArray = res.filter(item => !Boolean(exception.find((el) => {
 					return el === item.CharCode
 				})))
-				currenciesArray.sort((a, b) => a - b);
-				currenciesArray = [...{ ID: 'R1', Name: 'Российский рубль', CharCode: 'RUR', Nominal: 1, NumCode: 001, },...defaultCurrencyArray, ...currenciesArray]
+				currenciesArray.sort((a, b) => a.Name.localeCompare(b.Name));
+				currenciesArray = [{ ID: 'R1', Name: 'Российский рубль', CharCode: 'RUR', Nominal: 1, NumCode: '001', Value: 1 },...defaultCurrencyArray, ...currenciesArray]
 				console.log('ca > ', currenciesArray);
 			setCurrencies(currenciesArray);
 		})
--- src/components/CurrenciesForm.jsx
+++ src/components/CurrenciesForm.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
 const CurrenciesForm = ({ currencies }) => {
+	
 	return (
 		<div className='calc-currency'>
-			<div className='calc-currency-column'>{currencies.slice(0, 11).map(item =>
-				<div key={item.ID} className='calc-currency-row'>
+			<div className='calc-currency-column'>{currencies.slice(0, 11).map((item) => {
+				if (item.Name === 'Фунт стерлингов Соединенного королевства') {
+					item.Name = 'Фунт стерлингов'
+				}
+				return (<div key={item.ID} className='calc-currency-row'>
 					<div className='cacl-currency-name'>{item.Name}</div>
 					<div className='cacl-currency-sign'>{item.CharCode}</div>
-				</div>)}
+				</div>)
+			} 
+				)}
 			</div>
 			<div className='calc-currency-column calc-currency-column-center'>{currencies.slice(11, 22).map(item =>
 				<div key={item.ID} className='calc-currency-row'>
